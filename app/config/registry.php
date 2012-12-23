d1900e2b8bb4199fb43d844bb0eb2f588746292f
Benjamin Legrand <benjilegnard@protonmail.com>
2012-12-23 18:38:12 +0100

    resume initial commit

---
+++ app/config/registry.php
@@ -0,0 +1,6 @@
+<?php
+require ROOT.'/vendor/Slim/Slim/Slim.php';
+require ROOT.'/vendor/php-activerecord/ActiveRecord.php';
+require ROOT.'/vendor/Slim/Slim/View.php';
+require ROOT.'/vendor/Slim-Extras/Views/Twig.php';
+require ROOT.'/vendor/Slim-Extras/Log/DateTimeFileWriter.php';
---
+++ web/index.php
@@ -0,0 +1,3 @@
+<?php
+define('ROOT', dirname(__DIR__));
+require ROOT.'/app/index.php';
