b7c35d1bec8d7d5ae6ceecd1d01197346ac1aed0
Benjamin Legrand <benjilegnard@protonmail.com>
2013-03-22 22:38:56 +0100

    added brb old User boilerplate + jasmine specsrunner + phpunit test files

---
+++ tests/specs/SlidesSpecs.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Benjamin Legrand
+ * Date: 21/03/13
+ * Time: 10:36
+ * To change this template use File | Settings | File Templates.
+ */
+class SlidesSpecs
+{
+
+}
