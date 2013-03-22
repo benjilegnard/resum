145d6ee93939724bff006531c45bf54f43178624
Benjamin Legrand <benjilegnard@protonmail.com>
2013-03-22 22:00:57 +0100

    javascript libraries and svg files, and a first try on a Backbone architecture based on ngautier's article http://ngauthier.com/2012/02/raphael-backbone-traer.html

---
+++ public/js/resum.js
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Benjamin Legrand
+ * Date: 21/03/13
+ * Time: 11:12
+ * To change this template use File | Settings | File Templates.
+ */
+require.config({
+    baseUrl:'js/',
+    paths:[
+
+    ]
+});
+define(["jquery","backbone","raphael","bootstrap"],function($, Backbone, Raphael){
+
+});
