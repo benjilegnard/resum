261aacb0458cba771d5c25a3c340d3f8e4d5bf5c
Benjamin Legrand <benjilegnard@protonmail.com>
2015-08-20 17:39:03 +0200

    refactor data

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -3,6 +3,15 @@ var util = require( 'gulp-util' );
 var config = require( '../config.js' );
 var jade = require( 'gulp-jade' );
 
+function getData(){
+    return {
+        'pages':require('../../data/pages.json'),
+        'icons':require('../../data/icons.json'),
+        'jobs':require('../../data/jobs.json'),
+        'projects':require('../../data/projects.json'),
+        'skills':require('../../data/skills.json'),
+    }
+}
 
 gulp.task('html', function () {
     gulp.src(config.jade.files)
--- public/js/resum.js
+++ public/js/resum.js
@@ -1,27 +1,4 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Benjamin Legrand
- * Date: 21/03/13
- * Time: 11:12
- * To change this template use File | Settings | File Templates.
- */
-require.config({
-    baseUrl:'/public/js/',
-    paths:{
-        "zepto":"libs/zepto",
-        "lodash":"libs/lodash",
-        "oriDomi":"libs/oriDomi",
-        "noClickDelay":"libs/noClickDelay"
-    },
-    shims:{
-        "zepto":{"exports":"Zepto"},
-        "raphael":{"exports":"Raphael"},
-        "lodash":{"exports":"_"}
-    }
-});
-define(["zepto", "oriDomi","icons"], function ($, oriDomi, icons) {
-
-
+(function($){
     var resetSectionHeight =  function (e) {
         $('section').css("height", $(window).height());
     };
@@ -29,9 +6,9 @@ define(["zepto", "oriDomi","icons"], function ($, oriDomi, icons) {
     $(document).ready(function (e) {
         resetSectionHeight(e);
         $(window).on('resize', resetSectionHeight);
-        $('#menu ul').on('click', function (e) {
+        $('#menu').find('ul').on('click', function (e) {
             return false;
         });
     });
 
-});
+})(jQuery);
