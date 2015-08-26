b1a6e4685252071776cff6d54b6c3e8e1fe2d832
Benjamin Legrand <benjilegnard@protonmail.com>
2015-08-26 18:00:45 +0200

    added a zip task.

---
+++ gulp/tasks/zip.js
@@ -0,0 +1,12 @@
+
+var gulp = require('gulp');
+var util = require('gulp-util');
+var config = require('../config.js');
+var zip = require('gulp-zip');
+
+
+gulp.task('zip',['build-dev'], function () {
+    return gulp.src(config.destination+'/**/*')
+        .pipe(zip('resum.zip'))
+        .pipe(gulp.dest('./'));
+});
