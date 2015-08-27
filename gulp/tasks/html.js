61f272033c30dd63966575471ea9a2c3fd87a7b4
Benjamin Legrand <jibhaine@gmail.com>
2015-08-27 17:04:37 +0200

    use gulp-data with gulp-jade to feed json files to jade templates.

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -2,19 +2,22 @@ var gulp = require( 'gulp' );
 var util = require( 'gulp-util' );
 var config = require( '../config.js' );
 var jade = require( 'gulp-jade' );
+var data = require('gulp-data');
 
-function getData(){
+function getData(file){
     return {
         'pages':require('../../data/pages.json'),
         'icons':require('../../data/icons.json'),
-        'jobs':require('../../data/jobs.json'),
+        'jobs':require('../../data/experience.json'),
         'projects':require('../../data/projects.json'),
         'skills':require('../../data/skills.json'),
+        'slides':require('../../data/slides.json')
     }
 }
 
 gulp.task('html', function () {
     gulp.src(config.jade.files)
+        .pipe(data(getData))
         .pipe(jade({
             pretty: true
         }))
