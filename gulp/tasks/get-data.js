587d78abf1a97d40f0b7aafadaf354f7e34b4c9e
Benjamin Legrand <benjilegnard@protonmail.com>
2015-08-31 17:53:25 +0200

    refactored json read in a get-data.js file & added a source pdf to serve as a model for pdf-write.

---
+++ gulp/tasks/get-data.js
@@ -0,0 +1,10 @@
+module.exports = function(){
+    return {
+        'pages':require('../../data/pages.json'),
+        'icons':require('../../data/icons.json'),
+        'jobs':require('../../data/experience.json'),
+        'projects':require('../../data/projects.json'),
+        'skills':require('../../data/skills.json'),
+        'slides':require('../../data/slides.json')
+    }
+};
