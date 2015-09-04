c30dc642f7cb2dd217b30597959f60d5217c486d
Benjamin Legrand <jibhaine@gmail.com>
2015-09-04 16:54:01 +0200

    removed jquery, only vanilla JS.

--- gulp/tasks/jshint.js
+++ gulp/tasks/jshint.js
@@ -3,6 +3,8 @@ var gulp = require('gulp');
 var util = require('gulp-util');
 var config = require('../config.js');
 var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 var connect = require( 'gulp-connect' );
 
 gulp.task('lint', function () {
@@ -13,5 +15,8 @@ gulp.task('lint', function () {
 
 gulp.task('js-copy', function () {
     return gulp.src(config.source + '/js/**/*.js')
+    	.pipe(sourcemaps.init())
+    	.pipe(uglify())
+    	.pipe(sourcemaps.write())
         .pipe(gulp.dest(config.destination + '/js'))
 });
