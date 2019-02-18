aedf251d9420b83b5358bfdf82b3eeeb93986590
Benjamin Legrand <benjilegnard@protonmail.com>
2019-02-18 12:16:31 +0100

    feat(): set up docker dev env

---
+++ webpack.config.ts
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./client/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss?$/,
+        use: "sass-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
