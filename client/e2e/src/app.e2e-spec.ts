fced0c0324c131217bd4944c756dac3121d4fa64
Benjamin Legrand <benjilegnard@protonmail.com>
2019-08-25 19:17:14 +0200

    feat(angular): move resum to client folder and client to client-old

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to resum!');
+    expect(page.getTitleText()).toEqual('client app is running!');
   });
 
   afterEach(async () => {
