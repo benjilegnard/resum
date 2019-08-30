bd714c5b843294b01a9126af2c99aee861c51412
Benjamin Legrand <benjilegnard@protonmail.com>
2019-08-30 06:38:13 +0200

    chore: delete root files (code will be in client or server sub-projects)

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php require 'vendor/autoload.php';
-// instantiate the App object
-$app = new \Slim\App();
-// Add route callbacks
-$app->get('/', function ($request, $response, $args) {
-    return $response->withStatus(200)->write('Hello World!');
-});
-// Run application
-$app->run();
