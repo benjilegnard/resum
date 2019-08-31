da1d75a15ee6e26d0108856b8c585189922a2a16
Benjamin Legrand <jibhaine@free.fr>
2019-08-31 12:33:25 +0200

    feat(sass): add breakpoint-sass lib and media queries tests

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, SharedModule],
       declarations: [AppComponent],
     }).compileComponents();
   }));
