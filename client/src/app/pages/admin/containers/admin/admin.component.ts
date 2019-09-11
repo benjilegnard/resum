00ff6f242145d78e0a66f526d7e17e6eb0d901f1
Benjamin Legrand <jibhaine@free.fr>
2019-09-11 12:53:28 +0200

    feat(HMR): enable HMR

--- client/src/app/pages/admin/containers/admin/admin.component.ts
+++ client/src/app/pages/admin/containers/admin/admin.component.ts
@@ -11,6 +11,7 @@ export class AdminComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log('admin');
   }
 
 }
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootloader, hmrModule } from '@angularclass/hmr';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,5 +9,25 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+function basicBootstrap(AppModule) {
+  return platformBrowserDynamic().bootstrapModule(AppModule)
+    .then(MODULE_REF => {
+      console.log('Bootstrap success');
+
+      return MODULE_REF;
+    });
+  // .catch(err => console.error(err));
+}
+
+
+bootloader(() => {
+  const promise = platformBrowserDynamic().bootstrapModule(AppModule);
+
+  if (environment.hmr) {
+    ( module as any).hot.accept();
+
+    promise.then((mod) => {
+      return hmrModule(mod,  module as any);
+    });
+  }
+});
