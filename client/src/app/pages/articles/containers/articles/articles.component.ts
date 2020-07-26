f5a2790af429823ee615e3e95da86df9f9674fff
Benjamin Legrand <benjilegnard@protonmail.com>
2020-07-26 20:46:40 +0200

    STASH

--- client/src/app/pages/articles/containers/articles/articles.component.ts
+++ client/src/app/pages/articles/containers/articles/articles.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { of, Observable } from 'rxjs';
 
 @Component({
   selector: 'bl-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ArticlesComponent implements OnInit {
+  public articles: Observable<Array<any>> = of([]);
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
