3108ff750d61846eae81e8a2e4dc58fdf80760be
Benjamin Legrand <benjilegnard@protonmail.com>
2021-12-20 14:46:32 +0100

    feat(scully): add article page and md file

---
+++ client/data/articles/2021-12-13-anewstart.md
@@ -0,0 +1,21 @@
+---
+title: 2021-12-13-anewstart
+description: blog description
+published: false
+slugs:
+    - ___UNPUBLISHED___kxbkpejs_lDaUD7pydiP3eSNxzBVzU2uYuIno5nKF
+---
+
+# 2021-12-13-anewstart
+
+tralala pouet pouet
+
+| Label | Description |
+| ----- | ----------- |
+| label | description |
+
+```typescript
+function yolo(test: string) {
+    return "523"
+}
+```
---
+++ client/src/app/pages/articles/containers/article/article.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'bl-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ArticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
