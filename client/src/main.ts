08eea1583eea9d470c7567e73863d0dcb7a19fd3
Benjamin Legrand <benjilegnard@protonmail.com>
2022-09-16 17:26:25 +0200

    chore: remove @angularclass/hmr and hmrModule()

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,5 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { bootloader, hmrModule } from '@angularclass/hmr';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -9,25 +8,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-function basicBootstrap() {
-  return platformBrowserDynamic().bootstrapModule(AppModule)
-    .then(MODULE_REF => {
-      console.log('Bootstrap success');
-
-      return MODULE_REF;
-    });
-  // .catch(err => console.error(err));
-}
-
-
-bootloader(() => {
-  const promise = platformBrowserDynamic().bootstrapModule(AppModule);
-
-  if (environment.hmr) {
-    ( module as any).hot.accept();
-
-    promise.then((mod) => {
-      return hmrModule(mod,  module as any);
-    });
-  }
-});
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
