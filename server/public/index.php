1478eb2ba682b51f1a971491a2f4b301ed6652b3
Benjamin Legrand <benjilegnard@protonmail.com>
2022-11-16 11:52:50 +0100

    feat: change thumbnails images
    - 2022 picture
    - inverse the force of the color filter
    - fix docker compose port

--- server/public/index.php
+++ server/public/index.php
@@ -1,8 +1,8 @@
 <?php
 
-// todo remove later, deprecated warnings popups mainly because of intval being deprecated : 
+// todo remove later, deprecated warnings popups mainly because of intval being deprecated :
 // solution: either use another lib for graphics, or use gd directly
-// see: https://php.watch/versions/8.1/deprecate-implicit-conversion-incompatible-float-string 
+// see: https://php.watch/versions/8.1/deprecate-implicit-conversion-incompatible-float-string
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -25,19 +25,22 @@ $app->get('/thumbnails/{seed}.png', function (
     $background = __DIR__ . '/__assets__/thumbnail.png';
     $image->setDimensionsFromImage($background);
     $image->draw($background);
+
     // draw random stuff
     $maxWidth = 600;
     $maxHeight = 315;
     for ($i = 1; $i <= 50; $i++) {
         $x = mt_rand(0, $maxWidth);
         $y = mt_rand(0, $maxHeight);
-        $width = mt_rand(5, 50);
-        $opacity = mt_rand(10, 50) / 100;
+        $width = mt_rand(5, 100);
+        $opacity = mt_rand(5, 20) / 100;
         $image->circle($x, $y, $width, [255, 255, 255], $opacity);
     }
 
+    // additional filters
+    imagefilter($image->getResource(), IMG_FILTER_SMOOTH, 10);
+
     // colorize image
-    imagefilter($image->getResource(), IMG_FILTER_GAUSSIAN_BLUR, 0.2 );
     $colors = [
         [255, 0, mt_rand(0, 255)],
         [0, 255, mt_rand(0, 255)],
@@ -53,7 +56,7 @@ $app->get('/thumbnails/{seed}.png', function (
         $colors[$randomIndex][0],
         $colors[$randomIndex][1],
         $colors[$randomIndex][2],
-        0.1
+        110
     );
 
     // re-draw transparent image over generated one
