cf3d2e68c95dd36743d891ce63a05fab39209f57
Benjamin Legrand <benjilegnard@protonmail.com>
2023-08-12 11:23:01 +0200

    [WIP] feat: social links menu

---
+++ src/app/shared/model/social-link.ts
@@ -0,0 +1,40 @@
+export const links = [
+  { icon: 'twitch', label: 'Twitch', link: 'https://twitch.tv/benjilegnard' },
+  {
+    icon: 'youtube',
+    label: 'Youtube',
+    link: 'https://youtube.com/channel/UC1j-ULK7Dj0cfQD4dnDfWZg',
+  },
+  {
+    icon: 'instagram',
+    label: 'Instagram',
+    link: 'https://instagram.com/benjilegnard',
+  },
+  {
+    icon: 'facebook',
+    label: 'Facebook',
+    link: 'https://facebook.com/benjilegnard',
+  },
+  { icon: 'github', label: 'Github', link: 'https://github.com/benjilegnard' },
+  {
+    icon: 'codepen',
+    label: 'Codepen',
+    link: 'https://codepen.io/benjilegnard',
+  },
+  {
+    icon: 'reddit',
+    label: 'Reddit',
+    link: 'https://reddit.com/u/benjilegnard',
+  },
+  {
+    icon: 'linkedin',
+    label: 'Linkedin',
+    link: 'https://linkedin.com/in/benjilegnard/',
+  },
+];
+
+export interface SocialLink {
+  icon: string;
+  label: string;
+  link: string;
+}
