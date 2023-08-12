a4261b7f8a7b91d892dd30d09d22e8121fdbce75
Benjamin Legrand <benjilegnard@protonmail.com>
2023-08-12 11:22:37 +0200

    feat: add publication date to articles

--- client/src/app/pages/about.page.ts
+++ client/src/app/pages/about.page.ts
@@ -2,9 +2,19 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'bl-about',
-  template: `<h2>About me</h2>
+  template: `
+    <h2>About</h2>
+    <h3>About me</h3>
     <p>A small presentation text</p>
-    <p>Another small presentation text</p> `,
+    <h3>This blog</h3>
+    <p>This website, was made using :</p>
+    <ul>
+      <li>
+        Icons by
+        <a href="https://phosphoricons.com/" target="_blank">Phosphoricons</a>
+      </li>
+    </ul>
+  `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
--- client/src/app/pages/articles/[articleSlug].page.ts
+++ client/src/app/pages/articles/[articleSlug].page.ts
@@ -1,10 +1,6 @@
 import { MarkdownComponent, injectContent } from '@analogjs/content';
-import { AsyncPipe, NgIf } from '@angular/common';
-import {
-  Component,
-  ChangeDetectionStrategy,
-  Input,
-} from '@angular/core';
+import { AsyncPipe, DatePipe, NgIf } from '@angular/common';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 
 import 'prismjs';
 import 'prismjs/themes/prism.css';
@@ -13,22 +9,37 @@ import { ArticleAttributes } from '@benjilegnard/resum/shared/model';
 
 @Component({
   selector: 'bl-article',
-  template: `<div class="article-content">
-      <ng-container *ngIf="article$ | async as article">
-        <h1>{{ article.attributes.title }}</h1>
+  template: `
+    <ng-container *ngIf="article$ | async as article">
+      <p class="article-header">
+        <span class="article-date"
+          >{{ article.attributes.publishedAt | date }}
+        </span>
+      </p>
+      <div class="article-content">
         <analog-markdown [content]="article.content"></analog-markdown>
-      </ng-container>
-    </div>
-    <ul class="article-share-box">
-      <li></li>
-    </ul>`,
-  styles: [``],
-  imports: [MarkdownComponent, NgIf, AsyncPipe],
+      </div>
+      <ul class="article-share-box">
+        <li></li>
+      </ul>
+    </ng-container>
+  `,
+  styles: [
+    `
+      .article-header {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+      }
+      .article-content {
+      }
+    `,
+  ],
+  imports: [MarkdownComponent, NgIf, AsyncPipe, DatePipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
 })
 export class ArticlePageComponent {
-
   @Input()
   public slug!: string;
 
