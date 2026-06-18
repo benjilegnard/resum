f4f5a7b01b8ed9bbc538938db9d133cb9dd6e89b
Benjamin Legrand <benjilegnard@protonmail.com>
2026-06-18 11:36:03 +0200

    fix: bootstrap with context

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,6 +1,9 @@
 import '@angular/platform-server/init';
 import { enableProdMode } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
+import {
+  bootstrapApplication,
+  BootstrapContext,
+} from '@angular/platform-browser';
 import { renderApplication } from '@angular/platform-server';
 import { provideServerContext } from '@analogjs/router/server';
 import { ServerContext } from '@analogjs/router/tokens';
@@ -12,8 +15,8 @@ if (import.meta.env.PROD) {
   enableProdMode();
 }
 
-export function bootstrap() {
-  return bootstrapApplication(AppComponent, config);
+export function bootstrap(context: BootstrapContext) {
+  return bootstrapApplication(AppComponent, config, context);
 }
 
 export default async function render(
